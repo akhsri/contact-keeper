c86073bb7773927fc4fb30fe1dbe56bdc753fdb6
Aakash Srivastav <akhsri21@gmail.com>
2020-05-27 02:44:14 +0530

    first commit

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+  // Initializing context to have access to any state associated with contactcontext
+  const contactContext = useContext(ContactContext);
+
+  // Pulling out contacts from contactContext
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
+
+  if (contacts !== null && contacts.length === 0 && !loading) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {filtered !== null
+            ? filtered.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
+};
+
+export default Contacts;
